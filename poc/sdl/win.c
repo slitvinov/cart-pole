afdf43202b470e78c429147aa3f95d8b09d2c036
Sergey Litvinov <slitvinov@gmail.com>
2020-02-23 11:51:18 +0100

    add sleep

--- poc/sdl/win.c
+++ poc/sdl/win.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "SDL.h"
 
-int main(int argc, char **argv)
+int
+main(void)
 {
-  SDL_Window *window;  
-  SDL_Init(SDL_INIT_VIDEO);
-  SDL_Point window_position = {
-			       SDL_WINDOWPOS_CENTERED,
-			       SDL_WINDOWPOS_CENTERED
-  };
-  SDL_Point window_size = {640, 480};
-  window = SDL_CreateWindow( 
-			    "Window",
-			    window_position.x,
-			    window_position.y,
-			    window_size.x,
-			    window_size.y,
-			    SDL_WINDOW_OPENGL
-    char *c;
-    fscanf(stdin, "%c", &c);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+    unsigned int s;
+    SDL_Window *window;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Point window_position = {
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED
+    };
+    SDL_Point window_size = { 640, 480 };
+    window = SDL_CreateWindow("Window",
+                              window_position.x,
+                              window_position.y,
+                              window_size.x,
+                              window_size.y, SDL_WINDOW_OPENGL);
+    s = 5;
+    printf("sleep for %d seconds\n", s);
+    sleep(s);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
