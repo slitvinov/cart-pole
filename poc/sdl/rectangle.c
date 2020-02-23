5421f02d9182f2976e6a2417046d57be49ad8bdd
Sergey Litvinov <slitvinov@gmail.com>
2020-02-23 20:31:12 +0100

    use gfxPrimitives

--- poc/sdl/rectangle.c
+++ poc/sdl/rectangle.c
@@ -12,18 +12,23 @@ main(void)
     SDL_Rect rectangle;
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-	fprintf(stderr, "%s: SDL_Init failed: %s\n", me, SDL_GetError());
-	exit(2);
+        fprintf(stderr, "%s: SDL_Init failed: %s\n", me, SDL_GetError());
+        exit(2);
     }
-    window = SDL_CreateWindow("Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+    window =
+        SDL_CreateWindow("Window", SDL_WINDOWPOS_CENTERED,
+                         SDL_WINDOWPOS_CENTERED, 640, 480,
+                         SDL_WINDOW_OPENGL);
     if (window == NULL) {
-	fprintf(stderr, "%s: SDL_CreateWindow failed: %s\n", me, SDL_GetError());
-	exit(2);
+        fprintf(stderr, "%s: SDL_CreateWindow failed: %s\n", me,
+                SDL_GetError());
+        exit(2);
     }
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (renderer == NULL) {
-	fprintf(stderr, "%s: SDL_CreateRenderer failed: %s\n", me, SDL_GetError());
-	exit(2);
+        fprintf(stderr, "%s: SDL_CreateRenderer failed: %s\n", me,
+                SDL_GetError());
+        exit(2);
     }
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
@@ -35,7 +40,7 @@ main(void)
     rectangle.h = 150;
     SDL_RenderFillRect(renderer, &rectangle);
     SDL_RenderPresent(renderer);
-    
+
     seconds = 10;
     printf("sleep for %d seconds\n", seconds);
     SDL_Delay(seconds * 1000);
