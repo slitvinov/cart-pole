a2144cc839062c6410444cfc8cbd5ec3963a264d
Sergey Litvinov <slitvinov@gmail.com>
2020-02-23 11:46:04 +0100

    clean up

---
+++ poc/sdl/win.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "SDL.h"
+
+int main(int argc, char **argv)
+{
+  SDL_Window *window;  
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Point window_position = {
+			       SDL_WINDOWPOS_CENTERED,
+			       SDL_WINDOWPOS_CENTERED
+  };
+  SDL_Point window_size = {640, 480};
+  window = SDL_CreateWindow( 
+			    "Window",
+			    window_position.x,
+			    window_position.y,
+			    window_size.x,
+			    window_size.y,
+			    SDL_WINDOW_OPENGL
+    char *c;
+    fscanf(stdin, "%c", &c);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+}
