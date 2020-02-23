2b149b73d019ee9d1629c772c32fcea215a5d7ab
Sergey Litvinov <slitvinov@gmail.com>
2020-02-23 20:40:42 +0100

    clean up gfx

--- poc/sdl/win.c
+++ poc/sdl/win.c
@@ -10,17 +10,7 @@ main(void)
     SDL_Window *window;
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        fprintf(stderr, "%s: SDL_Init failed: %s\n", me, SDL_GetError());
-        exit(2);
-    }
-    window =
-        SDL_CreateWindow("Window", SDL_WINDOWPOS_CENTERED,
-                         SDL_WINDOWPOS_CENTERED, 640, 480,
-                         SDL_WINDOW_OPENGL);
-    if (window == NULL) {
-        fprintf(stderr, "%s: SDL_CreateWindow failed: %s\n", me,
-                SDL_GetError());
-        exit(2);
+exit(2);
     }
     seconds = 1;
     printf("sleep for %d seconds\n", seconds);
